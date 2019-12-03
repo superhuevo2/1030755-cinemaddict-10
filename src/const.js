abc81ecc9f8733d61ce797bec2a0151f83acc181
Anton <933375@gmail.com>
2019-12-03 14:14:46 +0300

    сделал моки

---
+++ src/const.js
@@ -0,0 +1,32 @@
+const Films = [
+  `Побег из шоушенка`,
+  `Зеленая миля`,
+  `Форрест Гамп`,
+  `Список Шиндлера`,
+  `1+1`,
+  `Начало`,
+  `Леон`,
+  `Король Лев`,
+  `Бойцовский клуб`,
+  `Иван Васильевич меняет профессию`,
+  `Жизнь прекрасна`,
+  `Достучаться до небес`,
+  `Крестный отец`,
+  `Криминальное чтиво`,
+  `Престиж`,
+];
+const Posters = [
+  `made-for-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
+const Genres = [`Drama`, `Film-Noir`, `Mystery`, `Action`, `Horror`, `Musical`, `Western`];
+const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+const YEAR_FROM = 1929;
+const YEAR_TO = 2020;
+
+export {Films, Posters, Genres, LOREM_IPSUM, YEAR_FROM, YEAR_TO}
